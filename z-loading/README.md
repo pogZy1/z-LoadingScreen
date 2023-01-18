52cb2e3becc6ae1fb3fffcde66c3d2942d47d099
pogZy1 <118043424+pogZy1@users.noreply.github.com>
2023-01-17 20:48:45 -0500

    Add files via upload

---
+++ z-loading/README.md
@@ -0,0 +1,12 @@
+Loading Screen For Bod Server
+
+
+My Discord : !pogZy💯#0001
+Github: https://github.com/pogZy1
+
+
+You can also change the music/img on the folder -> img and music 
+
+If any issues just send me a DM 
+
+ZeePo Developer On FiveM
---
+++ z-loading/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'ZeePo'
+description 'Discord:!pogZy💯#0001'
+version '1.0.0'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+files {
+    'index.html',
+    'css/style.css',
+    'img/background-1.png',
+    'img/background-2.png',
+    'img/background-3.png',
+    'js/script.js',
+    'music/music.mp3'
+}
